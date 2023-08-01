71594ad576c5d21f943198da627b4356b4acaa1f
MuhammadAwais47cs <Muhammadawais47cs@gmail.com>
2023-08-01 11:20:04 +0500

    qwertyuio

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -72,7 +72,7 @@ function Home() {
     { width: 300, itemsToShow: 1, pagination: false },
     { width: 550, itemsToShow: 3, itemsToScroll: 2, pagination: false },
     { width: 850, itemsToShow: 3, pagination: false },
-    { width: 1150, itemsToShow: 4, pagination: false, itemsToScroll: 2 },
+    { width: 1150, itemsToShow: 5, pagination: false, itemsToScroll: 2 },
     { width: 1450, itemsToShow: 5, pagination: false },
     { width: 1750, itemsToShow: 6, pagination: false },
   ];
