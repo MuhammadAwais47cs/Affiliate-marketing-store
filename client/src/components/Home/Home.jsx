80947b84a00059994e142737964e09cbe91086d4
MuhammadAwais47cs <Muhammadawais47cs@gmail.com>
2023-08-11 15:06:11 +0500

    changes

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Loader from "../layout/Loader/Loader";
 import Product from "./Product.jsx";
 import Slider from "react-slick";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import moment from "moment";
 import { FaPlaneDeparture } from "react-icons/fa";
 import Carousel, { consts } from "react-elastic-carousel";
@@ -19,18 +19,17 @@ import { baseurl } from "../../baseurl";
 import axios from "axios";
 import CouponPopUp from "./components/CouponPopUp";
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return <div className={className} onClick={onClick} />;
-}
+// function SampleNextArrow(props) {
+//   const { className, style, onClick } = props;
+//   return <div className={className} onClick={onClick} />;
+// }
 
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return <div className={className} onClick={onClick} />;
-}
+// function SamplePrevArrow(props) {
+//   const { className, style, onClick } = props;
+//   return <div className={className} onClick={onClick} />;
+// }
 
 function Home() {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { loading, error, products } = useSelector((state) => state.products);
   const { brands } = useSelector((state) => state.brands);
@@ -144,32 +143,6 @@ function Home() {
                         />
                       )
                   )}
-                {/*  <Link
-                          to=""
-                          className="col-md-3 pe-auto "
-                          onClick={modalToggle}
-                        >
-                          <div class="m-2 p-3 rounded-2 shadow d-flex flex-column align-items-center ">
-                            <img
-                              src={images.url}
-                              alt=""
-                              height="100"
-                              width="100"
-                            />
-                            <span
-                              class=" my-2 w-100 "
-                              style={{ "border-top": "2px grey dashed " }}
-                            ></span>
-                            <h5 class="card-title py-1">{name}</h5>
-                            <p class="card-text py-1">Some card's content.</p>
-                            <button
-                              onClick={modalToggle}
-                              class="btn btn-primary"
-                            >
-                              somewhere
-                            </button>
-                          </div>
-                      </Link> */}
               </div>
               <div className="row ustify-content-center    ">
                 <div className="col-11 h1 text-warning  text-start shadow-sm bg-white  my-3  p-3 rounded-3">
@@ -280,44 +253,6 @@ function Home() {
                 modalToggle={modalToggle}
               />
             }
-            {/*  <Modal show={isOpenModal} size="md" onHide={modalToggle}>
-              <Modal.Body className="mt-n4   rounded-3">
-                <div class=" p-4  h-75    d-flex flex-column align-items-center ">
-                  <img
-                    src={modalData?.images?.url}
-                    alt=""
-                    class="border-3   rounded-pill"
-                    height="100"
-                    width="100"
-                  />
-                  <h5 class="fs-5 my-1 ">{modalData?.description}</h5>
-                  <p class="fs-5 text-center">Voucher Code is : </p>
-                  <span className="btn btn-outline-info   my-1 w-75">
-                    {modalData?.code}{" "}
-                  </span>
-                  <p class=" my-1">
-                    Date of Expiry :{" "}
-                    <span className=" ">
-                      {" "}
-                      {moment(modalData?.expireDate).format("DD-MM-YYYY")}{" "}
-                    </span>
-                  </p>
-                  <Link
-                    to={`${
-                      modalData?.couponType === "Code" ? "" : modalData?.link
-                    }`}
-                    target={`${
-                      modalData?.couponType === "Code" ? "" : "_blank"
-                    }`}
-                    className="btn btn-danger opacity-75 text-white w-md-25 w-sm-50 rounded-pill px-2  "
-                  >
-                    {modalData?.couponType === "Code"
-                      ? modalData?.code
-                      : "Provider"}
-                  </Link>
-                </div>
-              </Modal.Body>
-                  </Modal>*/}
           </div>
           <div className="bg-white shadow container">
             <p className="col-11 h1 gridHeading text-warning gridHeading  text-start my-1  p-3 rounded-3">
