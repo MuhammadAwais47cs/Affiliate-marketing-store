0d7f0d9eb6cd300c667c3c881e268bff423af8a8
MuhammadAwais47cs <Muhammadawais47cs@gmail.com>
2023-09-21 11:12:56 +0500

    working on new feature

--- client/src/components/Home/components/Coupon.jsx
+++ client/src/components/Home/components/Coupon.jsx
@@ -1,33 +1,53 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { baseurl } from "../../../baseurl";
 
 const Coupon = ({ product, callBack, hitFromBrandPage }) => {
   const navigate = useNavigate();
-  const handleLinkClick = (e, link) => {
-    console.log("handleLinkClick-------");
-    if (e.ctrlKey || e.metaKey) {
-      // If Ctrl (or Cmd on Mac) is held while clicking the link, open in a new tab
-      return;
-    }
-    e.preventDefault();
+  // const handleLinkClick = (e, link) => {
+  //   console.log("handleLinkClick-------");
+  //   if (e.ctrlKey || e.metaKey) {
+  //     // If Ctrl (or Cmd on Mac) is held while clicking the link, open in a new tab
+  //     return;
+  //   }
+  //   e.preventDefault();
 
-    // Open the URL in a new tab
-    // window.open(link, "_blank");
-    // window.focus();
-    // Handle the navigation in your own way, for example using history.push
-    // history.push('');
-    // navigate(link);
+  //   // Open the URL in a new tab
+  //   // window.open(link, "_blank");
+  //   // window.focus();
+  //   // Handle the navigation in your own way, for example using history.push
+  //   // history.push('');
+  //   // navigate(link);
+  // };
+  const handleClick = (link, history) => {
+    console.log("history ", history);
+    // Close the current tab
+    window.close();
+    console.log("history ", history);
+
+    // Open a new tab with a specific link
+    const linkTab = window.open(link, "_blank");
+
+    // Open a new tab with the previous page from the browser's history
+    const historyTab = window.open("/", "_blank");
+    historyTab.focus();
+    // historyTab.document.write("<html><body>Going back...</body></html>");
+    // setTimeout(() => {
+    // historyTab.history.back();
+    // }, 1000); // Adjust the delay as needed
   };
+
   const { images, description, badge, name, link, _id } = product;
   return (
-    <Link
+    <div
       // onClick={hitFromBrandPage ? handleLinkClick : callBack}
       // onClick={(e) => {
       //   handleLinkClick(e, link);
       //   callBack();
       // }}
+      onClick={(e) => handleClick(link, window.history)}
       className="productCard position-relative col-md-3"
-      to={`${hitFromBrandPage ? `/brand/${_id}` : link}`}
+      // to={`${hitFromBrandPage ? `/brand/${_id}` : link}`}
       // to={`${modalData?.couponType === "Code" ? "" : modalData?.link}`}
       // target={`${!hitFromBrandPage && "_blank"}`}
     >
@@ -49,7 +69,7 @@ const Coupon = ({ product, callBack, hitFromBrandPage }) => {
       {/* <p className="shareIcon position-absolute top-0 end-0 me-md-2 me-lg-4 text-danger ">
       <FaRegShareSquare />
     </p> */}
-    </Link>
+    </div>
     // <Link to="" className="Coupon col-sm-2 col-lg-3 position-relative pe-auto ">
     //   <div
     //     className="my-2 p-2 rounded-2 shadow d-flex flex-column align-items-center "
