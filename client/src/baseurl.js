e369d1d2a65bb62968bacada9cf6b7b7c54cb392
MuhammadAwais47cs <Muhammadawais47cs@gmail.com>
2023-09-22 21:07:15 +0500

    changes

--- client/src/baseurl.js
+++ client/src/baseurl.js
@@ -1,4 +1,4 @@
 // module.exports.baseurl =
 // "https://affiliate-marketing-stores-e75c8b0261c1.herokuapp.com"; // Live Server
 module.exports.baseurl = "http://localhost:4000";
-// module.exports.baseurl = "http://192.168.1.109:4000";
+// module.exports.baseurl = "http://192.168.1.107:4000";
