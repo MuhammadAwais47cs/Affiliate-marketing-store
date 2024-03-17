f9d03a49d19b25ae5366a0a9f29c0e71d0e0720c
Muhammadawais47cs <Muhammadawais47cs@gmail.com>
2024-03-18 04:57:23 +0500

    integrate google analytice and change the fav icon

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
-
+import ReactGA from "react-ga4";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -16,6 +15,8 @@ const options = {
   position: positions.BOTTOM_CENTER,
   transitions: transitions.SCALE,
 };
+
+ReactGA.initialize("G-CP9KBHEXHG");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
